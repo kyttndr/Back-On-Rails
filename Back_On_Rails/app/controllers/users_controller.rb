cd459a1d4979a13cd900c020ff85e383745c8e70
Peiyuan Sun <peiyuan.sun@mail.utoronto.ca>
2018-10-13 18:48:59 -0400

    added user register form

--- Back_On_Rails/app/controllers/users_controller.rb
+++ Back_On_Rails/app/controllers/users_controller.rb
@@ -1,22 +1,39 @@
 class UsersController < ApplicationController
-    def index
-    end
+  def index
+  end
 
-    def new
-    end
+  def new
+    @user = User.new
+  end
 
-    def create
+  def create
+    @user = User.new(allowed_params)
+    if @user.save
+      flash[:notice] = "Thank you for signing up!"
+      redirect_to root_url
+    else
+      flash[:alert] = "Invalid Form!"
+      render :new
     end
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def edit
-    end
+  def edit
+  end
 
-    def update
-    end
+  def update
+  end
 
-    def destroy
-    end
+  def destroy
+  end
+
+  # PRIVATE FUNCTIONS
+  private
+
+  def allowed_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
 end
--- Back_On_Rails/app/models/user.rb
+++ Back_On_Rails/app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true, length: {within: 6..40}
 end
