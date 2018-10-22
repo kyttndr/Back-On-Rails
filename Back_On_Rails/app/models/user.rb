2ed8070629826d772e0e24374849ae7b0a94379a
Peiyuan Sun <peiyuan.sun@mail.utoronto.ca>
2018-10-22 16:36:57 -0400

    added a few links, modified form method from 'form_for' to 'form_with' to meet the latest version styles

--- Back_On_Rails/app/models/user.rb
+++ Back_On_Rails/app/models/user.rb
@@ -3,5 +3,4 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, confirmation: true, length: {within: 6..40}
 end
