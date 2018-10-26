02bf54268e462c1731950a7c91f46b28dd3175b0
常雪君 <tsuneyukikun@changxuejuns-MacBook-Pro.local>
2018-10-26 11:11:37 -0400

    add 2 attributes for items in migration

--- Back_On_Rails/db/migrate/20181010221622_create_items.rb
+++ Back_On_Rails/db/migrate/20181010221622_create_items.rb
@@ -1,7 +1,8 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.string :name
+      t.string :description
       t.timestamps
     end
   end
