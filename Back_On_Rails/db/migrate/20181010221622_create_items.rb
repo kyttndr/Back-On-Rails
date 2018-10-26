c3a40a08bf931a90d20b5fa74adb09846e31a67f
常雪君 <tsuneyukikun@changxuejuns-MacBook-Pro.local>
2018-10-26 11:13:43 -0400

    Revert "add 2 attributes for items in migration"
    This reverts commit 02bf54268e462c1731950a7c91f46b28dd3175b0.

--- Back_On_Rails/db/migrate/20181010221622_create_items.rb
+++ Back_On_Rails/db/migrate/20181010221622_create_items.rb
@@ -1,8 +1,7 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string :name
-      t.string :description
+
       t.timestamps
     end
   end
