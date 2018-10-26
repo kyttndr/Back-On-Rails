fab9a3a85e525c8720acdcac4939696cf9764492
Haotian Zhao <haotian.zhao@mail.utoronto.ca>
2018-10-26 10:50:34 -0400

    create the one to many association between items and users

--- Back_On_Rails/app/models/item.rb
+++ Back_On_Rails/app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
     ## TODO: find out how FK works and link to User table
     # WILL NEED TO USE ANOTHER MIGRATION TO ADD
     # ITEM ATTRIBUTES/COLUMNS
+    belongs_to :user
 end
