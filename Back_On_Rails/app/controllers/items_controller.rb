7d0876c48b60d81a35d1e0a8dc199efa1037ccbb
常雪君 <tsuneyukikun@changxuejuns-MacBook-Pro.local>
2018-10-26 14:24:09 -0400

    change in model of item. Add edit, index, new and show to items views and controller

--- Back_On_Rails/app/controllers/items_controller.rb
+++ Back_On_Rails/app/controllers/items_controller.rb
@@ -1,22 +1,56 @@
 class ItemsController < ApplicationController
-    def index
-    end
+  def index
+    @items = Item.all
+  end
 
-    def new
-    end
+  def new
+    @item = Item.new
+    #user in item will be implemented correctly when session is implemented
+    @item.user = User.first
+  end
 
-    def create
+  def create
+    @item = Item.new(allowed_params)
+    #user in item will be implemented correctly when session is implemented
+    @item.user = User.first
+    if @item.save
+      flash[:notice] = "Thank you for register!"
+      redirect_to item_path(@item)
+    else
+      flash[:alert] = "Invalid Form!"
+      render :new
     end
+  end
 
-    def show
-    end
+  def show
+    @item = Item.find(params[:id])
+  end
 
-    def edit
-    end
+  def edit
+    @item = Item.find(params[:id])
+  end
 
-    def update
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(allowed_params)
+      flash[:notice] = "Successfully updated"
+      redirect_to item_path(@item)
+    else
+      render 'edit'
     end
+  end
 
-    def destroy
-    end
+  def destroy
+    @item = Item.find(params[:id])
+      @item.destroy
+      flash[:notice] = "Deleted"
+      redirect_to items_path
+  end
+  
+  private
+  
+  def allowed_params
+    params.require(:item).permit(:name, :description)
+  end
+  
 end
--- Back_On_Rails/app/models/item.rb
+++ Back_On_Rails/app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ApplicationRecord
     # WILL NEED TO USE ANOTHER MIGRATION TO ADD
     # ITEM ATTRIBUTES/COLUMNS
     belongs_to :user
+    validates :name, presence: true, length: { minimum: 3}
+    validates :description, presence: true, length: { minimum: 10}
 end
