5ae7fb74d7712967f0c034666caa5c5d0e637c65
vilgovskiy <43548101+vilgovskiy@users.noreply.github.com>
2018-10-31 17:36:15 -0400

    edit/update profiles

--- Back_On_Rails/app/controllers/profiles_controller.rb
+++ Back_On_Rails/app/controllers/profiles_controller.rb
@@ -9,7 +9,27 @@ class ProfilesController < ApplicationController
     @profile = @user.profile.create(profile_params)
   end
 
+  def show
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    if @user.profile.update(profile_params)
+      redirect_to @user
+    else
+      render edit
+    end
+  end
+
   private
+
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :phone_number, :date_of_birth,
                                     :street_number, :street_name, :city, :province, :country)
