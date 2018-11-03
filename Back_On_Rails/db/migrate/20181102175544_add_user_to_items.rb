51d020c0e9706770589e11043441117c6e31f0ff
xunathan <nathaniel.xu@mail.utoronto.ca>
2018-11-03 14:09:51 -0400

    fixed duplicate user_id db column issue. Please run rails db:migrate VERSION=0 and then rails db:migrate

--- Back_On_Rails/db/migrate/20181102175544_add_user_to_items.rb
+++ Back_On_Rails/db/migrate/20181102175544_add_user_to_items.rb
@@ -1,5 +1,6 @@
 class AddUserToItems < ActiveRecord::Migration[5.2]
   def change
-    add_reference :items, :user, foreign_key: true
+    add_reference :items, :user, foreign_key: true, index: true
+    add_foreign_key :items, :users
   end
 end
--- Back_On_Rails/db/schema.rb
+++ Back_On_Rails/db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_26_182344) do
+ActiveRecord::Schema.define(version: 2018_11_02_175544) do
 
   create_table "items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.string "name"
     t.string "description"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
