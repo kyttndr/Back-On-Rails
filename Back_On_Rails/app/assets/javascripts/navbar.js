55575edb43535f617251c0cffea4a2c38099cc13
xunathan <nathaniel.xu@mail.utoronto.ca>
2018-11-22 03:27:28 -0500

    changed jquery to work in turolinks

--- Back_On_Rails/app/assets/javascripts/navbar.js
+++ Back_On_Rails/app/assets/javascripts/navbar.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).on('turbolinks:load', function(){
     var unread_count = $('#unread_count').val();
     if (unread_count > 0) {
         $('.glyphicon-bell').css({'color': '#ff5050'});
