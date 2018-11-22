3e74d5b2311a9306a06a4d72f7bdf1e092733289
xunathan <nathaniel.xu@mail.utoronto.ca>
2018-11-22 18:54:17 -0500

    formatted the error messages for transactions and reviews

--- Back_On_Rails/app/assets/javascripts/transactions.js
+++ Back_On_Rails/app/assets/javascripts/transactions.js
@@ -3,3 +3,15 @@ $(document).on('turbolinks:load', function(){
     $('[data-toggle="tooltip"]').tooltip();
     $('[data-toggle="table"]').table();
 });
+
+/*
+$(document).ready(function(){
+    var error_message_html = $('.error_messages').html();
+    if(error_message_html != undefined){
+        //confirm(error_message_html);
+        $("#myModal").modal('show');
+
+    }
+
+});
+*/
