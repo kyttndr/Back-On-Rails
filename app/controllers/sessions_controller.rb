19ab9439c286ca03de490b999b03a5c6d3c97f1a
vilgovskiy <43548101+vilgovskiy@users.noreply.github.com>
2018-11-27 00:40:07 -0500

    Session adjustment and db additions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController<ApplicationController
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
 
-		if user && user.authenticate(params[:session][:password])
+		if user && user.activated && user.authenticate(params[:session][:password])
 			session[:user_id] = user.id
 			flash[:success] = "You have successfully logged in"
 			redirect_to root_path
