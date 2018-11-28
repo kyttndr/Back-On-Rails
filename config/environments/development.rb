2d39fa27f82f341c60b81d538fcdfdaf301f4de7
xunathan <nathaniel.xu@mail.utoronto.ca>
2018-11-28 00:33:32 -0500

    accidentally changed development mailer option.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   # Change this in production
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
