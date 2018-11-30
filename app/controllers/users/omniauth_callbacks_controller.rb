ef1322138b1ac6d46a66b65113832ca22495352e
Peiyuan Sun <peiyuan.sun@mail.utoronto.ca>
2018-11-30 18:12:54 -0500

    fixed a syntax error

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,35 +1,34 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    def facebook
-      @user = User.from_omniauth(request.env["omniauth.auth"])
+  def facebook
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
-      #if @user.persisted?
-        #sign_in_and_redirect @user, :event => :authentication
-      #else
-        #session["devise.facebook_data"] = request.env["omniauth.auth"]
-        #session[:user_id] = @user.id
-        #redirect_to new_user_registration_url
-      #end
-      #if @user.persisted?
-        #session[:user_id] = @user.id
-        #redirect_to root_path
-      #else
-        #session[:user_id] = @user.id
-        #redirect_to new_user_profile_path(@user)
-      #end
-      if @user.profile.nil?
-        session[:user_id] = @user.id
-        redirect_to new_user_profile_path(@user)
-        #UserMailer.account_activation(@user).deliver_now
-        #redirect_to root_path
-      else
-        session[:user_id] = @user.id
-        redirect_to root_path
-      end
-    end
-
-    def failure
+    #if @user.persisted?
+      #sign_in_and_redirect @user, :event => :authentication
+    #else
+      #session["devise.facebook_data"] = request.env["omniauth.auth"]
+      #session[:user_id] = @user.id
+      #redirect_to new_user_registration_url
+    #end
+    #if @user.persisted?
+      #session[:user_id] = @user.id
+      #redirect_to root_path
+    #else
+      #session[:user_id] = @user.id
+      #redirect_to new_user_profile_path(@user)
+    #end
+    if @user.profile.nil?
+      session[:user_id] = @user.id
+      redirect_to new_user_profile_path(@user)
+      #UserMailer.account_activation(@user).deliver_now
+      #redirect_to root_path
+    else
+      session[:user_id] = @user.id
       redirect_to root_path
     end
+  end
+
+  def failure
+    redirect_to root_path
   end
 
   def failure
