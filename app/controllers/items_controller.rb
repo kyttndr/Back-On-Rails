f1cc21a2a44c40a7f95295ae3146ffa25970afcb
常雪君 <tsuneyukikun@changxuejuns-MacBook-Pro.local>
2018-12-01 18:26:38 -0500

    now user can search for items without logging in

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :update, :show, :destroy]
-  before_action :require_user, except: [:index, :show]
+  before_action :require_user, except: [:index, :show, :search_items, :search]
   before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
