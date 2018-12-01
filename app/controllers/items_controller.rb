fb5d7334dccb65c5dcadbf90a422e0e01d8992bf
常雪君 <tsuneyukikun@changxuejuns-MacBook-Pro.local>
2018-12-01 17:55:23 -0500

    uncomment index method in item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,9 @@ class ItemsController < ApplicationController
   before_action :require_user, except: [:index, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
 
-  #def index
-    #@items = Item.paginate(page: params[:page], per_page: 5)
-  #end
+  def index
+    @items = Item.paginate(page: params[:page], per_page: 5)
+  end
 
   def new
     @item = Item.new
