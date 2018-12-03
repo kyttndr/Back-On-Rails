bb4a397ff25077edbf22e0dce12ac31a4c1776b2
Haotian Zhao <haotian.zhao@mail.utoronto.ca>
2018-12-03 14:53:30 -0500

    Added tests for friends and admin. Added validations on friendship

--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class FriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "Cannot build friendship with yourself" do
+    user = User.new
+    user.username = "test"
+    user.email = "testing@testing.com"
+    user.password = "12345678"
+    user.activated = true
+
+    user.save
+  
+    assert_not(user.friends << user)
+  end
+  
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,16 +14,25 @@ class UserTest < ActiveSupport::TestCase
     assert_not user.save
   end
 
+  test "Email format has to be valid" do
+    user = User.new
+    user.username = "test2"
+    user.email = "testing"
+    user.password = "12345678"
+    user.activated = true
+    assert_not user.save
+  end
+  
   test "Cannot save user with duplicate email" do
     user = User.new
-    user.username = "test1"
+    user.username = "test3"
     user.email = "testing@testing.com"
     user.password = "12345678"
     user.activated = true
     user.save
 
     user2 = User.new
-    user2.username = "test2"
+    user2.username = "test4"
     user2.email = "testing@testing.com"
     user2.password = "11111111"
     user2.activated = true
