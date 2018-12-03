7986134cd3561ce61dc49f212ef78ae46ae1618d
Peiyuan Sun <peiyuan.sun@mail.utoronto.ca>
2018-12-02 20:29:27 -0500

    fixed testing error, and added tests for places

--- test/models/place_test.rb
+++ test/models/place_test.rb
@@ -4,4 +4,16 @@ class PlaceTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "Cannot save place with no address" do
+    place = Place.new
+    assert_not place.save
+  end
+
+  test "Cannot save place with invalid address" do
+    place = Place.new
+    place.address = "adsjfaklsjfasnaldjaskldcj"
+    assert_not place.save
+  end
+
 end
